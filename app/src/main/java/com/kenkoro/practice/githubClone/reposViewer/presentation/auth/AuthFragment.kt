77c77577790c6e1bb285586202b6d5c5d4562069
kenkoro <frogharvard@gmail.com>
2024-12-03 00:55:44 +0700

    feat(auth-fragment): completed auth ui

--- app/src/main/java/com/kenkoro/practice/githubClone/reposViewer/presentation/auth/AuthFragment.kt
+++ app/src/main/java/com/kenkoro/practice/githubClone/reposViewer/presentation/auth/AuthFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.kenkoro.projects.githubClone.databinding.AuthFragmentBinding
 
 class AuthFragment : Fragment() {
   private var _binding: AuthFragmentBinding? = null
   private val binding: AuthFragmentBinding
     get() = _binding!!
+
+  private lateinit var tietAuthToken: TextInputEditText
+  private lateinit var tilAuthToken: TextInputLayout
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -26,6 +32,13 @@ class AuthFragment : Fragment() {
     savedInstanceState: Bundle?,
   ) {
     super.onViewCreated(view, savedInstanceState)
+    tietAuthToken = binding.tietAuthToken
+    tilAuthToken = binding.tilAuthToken
+
+    tietAuthToken.doOnTextChanged { text, start, before, count ->
+      tilAuthToken.error = "Invalid token"
+      tilAuthToken.errorIconDrawable = null
+    }
   }
 
   override fun onDestroyView() {
